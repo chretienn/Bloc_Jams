07c925eedb9610f191c385a27b2525238be3725c
chretienn <christianjdomingo@gmail.com>
2019-06-07 14:42:26 -0700

    sliders completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
     $('button#play-pause').attr('playState', player.playState);
   });
@@ -11,7 +11,7 @@
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
       const nextSong = album.songs[nextSongIndex];
-      player.playPause(nextSong);
+      helper.playPauseAndUpdate(nextSong);
 });
 
     $('button#previous').on('click', function() {
@@ -19,8 +19,25 @@
   const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
   const nextSongIndex = currentSongIndex - 1;
   if (nextSongIndex >= album.songs.length) { return; }
-  const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    const nextSong = album.songs[nextSongIndex];
+    helper.playPauseAndUpdate(nextSong);
   });
+
+  $('#time-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
+   });
+
+    $('#volume-control input').on('input', function (event) {
+      player.setVolume(event.target.value);
+    });
+
+    setInterval( () => {
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+    }, 1000);
 
 }
